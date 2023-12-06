e3b8ccb7552691c317f5475933e7f518526b2f90
Leonardo Jacon <114886011+jaconGon@users.noreply.github.com>
2023-12-06 08:25:04 -0300

    Delete ZooBoolHtmlsEManutencao directory

--- ZooBoolHtmlsEManutencao/ZooBool/src/main/java/br/com/unifal/zoobool/repository/FuncionarioRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.unifal.zoobool.repository;
-
-import br.com.unifal.zoobool.entity.Funcionario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FuncionarioRepository extends JpaRepository<Funcionario, Integer> {
-}
--- ZooBoolHtmlsEManutencao/ZooBool/src/main/java/br/com/unifal/zoobool/repository/ManutencaoRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.unifal.zoobool.repository;
-
-import br.com.unifal.zoobool.entity.Manutencao;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ManutencaoRepository extends JpaRepository<Manutencao, Integer> {
-}
